42c34515d7865e160b5eb96b5d7d23f5bc2d0c8a
RyoMatsuda@RCO <ryo_matsuda@r.recruit.co.jp>
2019-09-29 19:38:08 +0900

    make auth page

--- drf-vue-sample/frontend/src/router.ts
+++ drf-vue-sample/frontend/src/router.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Home from './views/Home.vue';
+import store from '@/store';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -20,6 +22,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/book',
@@ -28,3 +31,35 @@ export default new Router({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.getters['auth/isLoggedIn'];
+  const token = localStorage.getItem('api_token');
+  console.log('to.path=', to.path);
+
+  if (!to.matched.some(record => record.meta.requiresAuth)) {
+    next();
+  } else {
+    if (isLoggedIn) {
+      next();
+    } else {
+      // When Login is necessary but User isn't
+      if (token != null) {
+        // store.dispatch('auth/reload');
+        next();
+      } else {
+        forceToLoginPage(to, from, next);
+      }
+    }
+  }
+});
+
+function forceToLoginPage(to, from, next): void {
+  next({
+    path: '/book',
+    query: { next: to.fullPath },
+  });
+}
+
+
+export default router;
